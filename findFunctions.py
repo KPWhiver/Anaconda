270f5efadd236b9144771c343ba659955424339c
Sab Eros <mini.game@hotmail.com>
2013-04-17 16:21:05 +0200

    Started expanding register tracking

--- findFunctions.py
+++ findFunctions.py
@@ -9,7 +9,7 @@ from androlyze import *
 from tools import *
 
 # Parse the last argument of a function call
-def parseCall(call) :
+def parseCall(call):
     if call != '' and call[0] == '[':
         return '', ''
     
@@ -21,7 +21,7 @@ def parseCall(call) :
     return match.group(1), match.group(2)
  
 # Read the list of api sources       
-def sources(filename) :
+def sources(filename):
     with open(filename) as sourcesFile:
         sources = sourcesFile.readlines()
         
@@ -36,21 +36,55 @@ def sources(filename) :
     return functions
         
 # these functions are here to keep my head clear
-def method(idx, analysis) :
+def method(idx, analysis):
     return analysis.get_method(analysis.get_vm().get_methods()[idx])
 
-def block(idx, method) :
+def block(idx, method):
     return method.get_basic_blocks().get()[idx]
 
-def instruction(idx, block) :
+def instruction(idx, block):
     return block.get_instructions()[idx]
 
+def parseInstruction(instruction):
+    if 'invoke' in instruction.opcode():
+        instructionClass = structure.classByName(instruction.classAndmethod())
+        if not (instructionClass is None):
+            instructionMethod = instructionClass.methodByName(instruction.classAndMethod())
+            if not (instructionMethod is None):
+                print('method found')
+                return
+                # Parameter p* is tainted in method instructionMethod
+                
+        # class is not defined within APK
+        print('method not found')
+        
+    elif 'put' in instruction.opcode():
+        #Value is put inside array, 'aput'
+        #Value is put in instance field, 'iput'
+        #Value is put in static field, 'sput'
+        
+    elif 'move' in instruction.opcode():
+        #Value is moved into other register, 'move'
+        
+    else:
+        print('Unknown operation performed')
+        print(instruction.classAndMethod)
+    
+    
 
-def trackFromCall(method, register, blockIdx, instructionIdx) :
-    for block in method.blocks()[blockIdx:] :
+def trackFromCall(method, register, blockIdx, instructionIdx):
+    print "Tracking register", register
+    for block in method.blocks()[blockIdx:]:
         startIdx = instructionIdx if block == method.blocks()[blockIdx] else 0
-        for instruction in block.get_instructions()[startIdx:] :
-            print 'track'
+        for instruction in block.get_instructions()[startIdx:]:
+            if register in instruction.get_output():
+                
+                if instruction.get_name() in ['move-result-object', 'move-result', 'move-result-wide']:
+                    return # register is overwritten
+                
+                print instruction.get_name(), instruction.get_output()
+                
+    print("\n")
      
 def analyzeBlocks(method, classAndFunctions):
     
@@ -76,25 +110,25 @@ def analyzeBlocks(method, classAndFunctions):
                 elif className == 'java/net/Socket' and methodName == '<init>':
                     print className, methodName
                     
-            if instruction.get_name() in ['move-result-object', 'move-result', 'move-result-wide'] and previousWasSource :
-                trackFromCall(method, instructionArgs[0], blockIdx, instructionIdx)
+            if instruction.get_name() in ['move-result-object', 'move-result', 'move-result-wide'] and previousWasSource:
+                trackFromCall(method, instructionArgs[0], blockIdx, instructionIdx + 1)
 
 
-def main() :
+def main():
 
     classAndFunctions = sources('api_sources.txt')
 
-    structure = APKstructure('apks/LeakTest1.apk')
+    global structure = APKstructure('apks/LeakTest1.apk')
 
     
 
     # search through all classes
-    for _, jvmClass in structure.classes().items() :
+    for _, jvmClass in structure.classes().items():
     
         # search through all methods
-        for _, method in jvmClass.methods().items() :
+        for _, method in jvmClass.methods().items():
     
-            if not method.hasCode() :
+            if not method.hasCode():
                 continue
     
             analyzeBlocks(method, classAndFunctions)
