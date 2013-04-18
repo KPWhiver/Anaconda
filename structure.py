64bed2deb67f8d2f52cd6d8c3396a0bbaf07f7a7
Sab Eros <mini.game@hotmail.com>
2013-04-18 17:11:53 +0200

    Updated findFunctions to use the new structure search methods

--- structure.py
+++ structure.py
@@ -110,7 +110,7 @@ class Method:
         list = []
         for blockIdx, block in enumerate(self.d_blocks):
             for instructionIdx, instruction in enumerate(block.instructions()):
-                if instruction.opCode() in invokeOpcodes and className in instruction.parameters()[-2] and methodName in instruction.parameters()[-1]:                
+                if instruction.opcode() in invokeOpcodes and className in instruction.parameters()[-2] and methodName in instruction.parameters()[-1]:                
                     list.append([blockIdx, instructionIdx])
         
         return list
